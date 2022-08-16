62c6bf8c4c100cd0e1a340546d0642e82c63da4f
vbox <trufo2@gmail.com>
2022-08-16 15:42:39 +0200

    completed CRUD on Todos

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,12 +1,60 @@
 class MainController {
-    
-    example(req, res){
-        res.render("example.ejs", {
-            text: "This is an example API Route"
+    index(req, res){
+        const Todo = req.models.Todo
+        Todo.find({}, (er, todos) => {
+            if (er) {
+                res.status(400).send(er)
+            } else {
+                res.render("index.ejs", {todos})
+            }
         })
     }
-
-
+    new(req, res){
+        res.render("new.ejs")
+    }
+    create(req, res){
+        const Todo = req.models.Todo
+        req.body.completed = false
+        Todo.create(req.body, (er, todo) => {
+            if (er) {
+                res.status(400).send(er)
+            } else {
+                res.redirect("/")
+            }
+        })
+    }
+    show(req, res){
+        const id = req.params.id
+        const Todo = req.models.Todo
+        Todo.findById(id, (er, todo) => {
+            if (er) {
+                res.status(400).send(er)
+            } else {
+                res.render("show.ejs", {todo})
+            }
+        })
+    }
+    complete(req, res){
+        const id = req.params.id
+        const Todo = req.models.Todo
+        Todo.findByIdAndUpdate(id, {completed: true}, {new: true}, (er, todo) => {
+            if (er) {
+                res.status(400).send(er)
+            } else {
+                res.redirect("/")
+            }
+        })
+    }
+    destroy(req, res){
+        const id = req.params.id
+        const Todo = req.models.Todo
+        Todo.findByIdAndDelete(id, (er, todo) => {
+            if (er) {
+                res.status(400).send(er)
+            } else {
+                res.redirect("/")
+            }
+        })
+    }
 }
-
 export default MainController
